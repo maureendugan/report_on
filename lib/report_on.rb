67b6a5f894b009b992c0c49dc29c66bae10f8a71
moedu23 <maureen.k.dugan@gmail.com>
2015-02-08 17:17:31 -0800

    Design interface for github profile activity

--- lib/report_on.rb
+++ lib/report_on.rb
@@ -1,5 +1,10 @@
 module ReportOn
   def self.report(username)
-    "botandrose committed code today."
+    activity = Activity.new(username)
+    if activity.code_today?
+      "#{username} committed code today."
+    else
+      "#{username} did not commit code today."
+    end
   end
 end
--- spec/report_on_spec.rb
+++ spec/report_on_spec.rb
@@ -2,9 +2,22 @@ require 'report_on'
 
 describe ReportOn do
   describe '#report' do
+    before do
+      @fake_activity = double
+      fake_class = double(new: @fake_activity)
+      stub_const "ReportOn::Activity", fake_class
+    end
+
     it 'will report if a user committed code today.' do
+      @fake_activity.stub(code_today?: true)
       output = ReportOn.report("botandrose")
       expect(output).to eq "botandrose committed code today."
+    end
+
+    it 'will report if a user did not commit code today.' do
+      @fake_activity.stub(code_today?: false)
+      output = ReportOn.report("erniedog")
+      expect(output).to eq "erniedog did not commit code today."
     end
   end
 end
