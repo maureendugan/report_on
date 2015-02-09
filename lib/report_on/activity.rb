e5091d0f4de67fb772b3f97d2295204a1b9aaddc
moedu23 <maureen.k.dugan@gmail.com>
2015-02-08 18:32:23 -0800

    Silence standard error during curl

--- lib/report_on/activity.rb
+++ lib/report_on/activity.rb
@@ -9,8 +9,8 @@ module ReportOn
 
     def code_today?
       # load twice to make sure that the commit graph is generated
-      `curl https://github.com/#{username}`
-      response = `curl https://github.com/#{username}`
+      `curl https://github.com/#{username} 2>/dev/null`
+      response = `curl https://github.com/#{username} 2>/dev/null`
       if response.include? 'data-count="0" data-date="2015-02-08"'
         false
       else
